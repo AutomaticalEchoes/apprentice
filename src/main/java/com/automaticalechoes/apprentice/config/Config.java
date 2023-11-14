73091d6348bf74140a47aa13fdf3046bca7ba072
1121665965 <1121665965@qq.com>
2023-11-14 18:00:29 +0800

    功能增加，bug修复，版本更迭

--- src/main/java/com/automaticalechoes/apprentice/config/Config.java
+++ src/main/java/com/automaticalechoes/apprentice/config/Config.java
@@ -28,33 +28,35 @@ public class Config {
 
     static {
         BUILDER.push("master piece config");
+        BUILDER.comment("trade cost = TradeCostBase + random(TradeCostRandom)");
         BUILDER.push("trade of repair");
-        REPAIR_MAX_USE = BUILDER.defineInRange("max trade time",20,5,20);
-        REPAIR_COST_BASE = BUILDER.defineInRange("trade base cost",5,5,10);
-        REPAIR_COST_RANDOM = BUILDER.defineInRange("trade cost = base cost + Random(randomCost)",12,5,20);
-        REPAIR_BASE_PRE_COST = BUILDER.comment("repair base = basePreCost * cost").defineInRange("basePreCost",10,5,25);
-        REPAIR_MUT_PRE_LEVEL = BUILDER.comment("repair durability = base * mut * villagerLevel").defineInRange("mut pre level",0.40F,0.20F,0.40F);
+        REPAIR_MAX_USE = BUILDER.defineInRange("tradeMaxUse",20,5,20);
+        REPAIR_COST_BASE = BUILDER.defineInRange("tradeCostBase",5,5,10);
+        REPAIR_COST_RANDOM = BUILDER.defineInRange("tradeCostRandom",12,5,20);
+        BUILDER.comment("repair durability = (basePreCost + (mutPreLevel * villagerLevel) ) * cost");
+        REPAIR_BASE_PRE_COST = BUILDER.defineInRange("repairBasePreCost",10,5,25);
+        REPAIR_MUT_PRE_LEVEL = BUILDER.defineInRange("mutPreLevel",0.40F,0.20F,0.40F);
         BUILDER.pop();
 
         BUILDER.push("trade of record");
-        RECORD_MAX_USE = BUILDER.defineInRange("max trade time",20,5,20);
-        RECORD_COST_BASE = BUILDER.defineInRange("trade cost base",14,10,20);
-        RECORD_COST_RANDOM = BUILDER.defineInRange("trade cost random",17,15,20);
+        RECORD_MAX_USE = BUILDER.defineInRange("tradeMaxUse",20,5,20);
+        RECORD_COST_BASE = BUILDER.defineInRange("tradeCostBase",14,10,20);
+        RECORD_COST_RANDOM = BUILDER.defineInRange("tradeCostRandom",17,15,20);
         BUILDER.pop();
 
         BUILDER.push("trade of improve");
-        IMPROVE_MAX_USE = BUILDER.defineInRange("max trade time",3,1,5);
-        IMPROVE_COST_BASE = BUILDER.defineInRange("trade cost base",48,40,64);
-        IMPROVE_COST_RANDOM = BUILDER.defineInRange("trade cost random",8,0,32);
-        IMPROVE_MODIFIER_BASE = BUILDER.defineInRange("modifierBase",5,1,10);
-        IMPROVE_MODIFIER_RANDOM = BUILDER.defineInRange("modifierRandomMax",5,1,10);
-        BUILDER.push("modifier = (modifierBase + modifierRandom) / 10.\n Example: Damage = WeaponsBaseDamage * (1 + modifier)");
+        IMPROVE_MAX_USE = BUILDER.defineInRange("tradeMaxUse",3,1,5);
+        IMPROVE_COST_BASE = BUILDER.defineInRange("tradeCostBase",48,40,64);
+        IMPROVE_COST_RANDOM = BUILDER.defineInRange("tradeCostRandom",8,0,32);
+        IMPROVE_MODIFIER_BASE = BUILDER.defineInRange("modifierBase",3,1,10);
+        IMPROVE_MODIFIER_RANDOM = BUILDER.defineInRange("modifierRandom",5,1,10);
+        BUILDER.comment("modifier = (modifierBase + modifierRandom) / 10.\n Example: Damage = WeaponsBaseDamage * (1 + modifier)");
         BUILDER.pop();
 
         BUILDER.push("trade of fresh");
-        FRESH_MAX_USE = BUILDER.defineInRange("max trade time",20,5,20);
-        FRESH_COST_BASE = BUILDER.defineInRange("trade cost base",15,10,20);
-        FRESH_COST_RANDOM = BUILDER.defineInRange("trade cost random",8,0,10);
+        FRESH_MAX_USE = BUILDER.defineInRange("tradeMaxUse",20,5,20);
+        FRESH_COST_BASE = BUILDER.defineInRange("tradeCostBase",10,10,20);
+        FRESH_COST_RANDOM = BUILDER.defineInRange("tradeCostRandom",10,0,20);
         FRESH_COST_LEVEL_BONUS = BUILDER.comment("fresh trade cost = base + random + level * bonus").defineInRange("bonusPreLevel", 2,1,4);
         BUILDER.pop();
 
